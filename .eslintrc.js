eb7834e473bfba642ebe9ac18d98f672865d28cf
melt_adzuki <rebanira02@gmail.com>
2022-02-15 17:04:35 +0900

    インデントをタブに強制

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,57 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    "@nuxtjs/eslint-config-typescript",
-    "plugin:vue/essential",
-    "@vue/airbnb",
-  ],
-  settings: {
-    "import/resolver": {
-      alias: {
-        map: [
-          ["@", "."],
-          ["~", "."],
-        ],
-        extensions: [".vue", ".ts", ".js"],
-      },
-    },
-  },
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {
-    quotes: [
-      "error",
-      "double",
-    ],
-    semi: [
-      "error",
-      "never",
-    ],
-    "brace-style": [
-      "error",
-      "allman",
-    ],
-    "max-len": 0,
-    "class-methods-use-this": 0,
-  },
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		"@nuxtjs/eslint-config-typescript",
+		"plugin:vue/essential",
+		"@vue/airbnb",
+	],
+	settings: {
+		"import/resolver": {
+			alias: {
+				map: [
+					["@", "."],
+					["~", "."],
+				],
+				extensions: [".vue", ".ts", ".js"],
+			},
+		},
+	},
+	plugins: [
+		"pug",
+	],
+	// add your custom rules here
+	rules: {
+		indent: [
+			"error",
+			"tab",
+		],
+		"vue/html-indent": [
+			"error",
+			"tab",
+		],
+		"no-tabs": [
+			"error",
+			{
+				allowIndentationTabs: true,
+			},
+		],
+		quotes: [
+			"error",
+			"double",
+		],
+		semi: [
+			"error",
+			"never",
+		],
+		"brace-style": [
+			"error",
+			"allman",
+		],
+		"max-len": 0,
+		"class-methods-use-this": 0,
+	},
 }
